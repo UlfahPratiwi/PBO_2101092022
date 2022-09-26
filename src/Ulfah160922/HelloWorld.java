0d91a2ceed1d0574be9746b6a48987039a482fe2
UlfahPratiwi <ulfahpratiwi.bkt@gmail.com>
2022-09-26 12:04:37 +0700

    Initial commit

---
+++ src/Ulfah160922/HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ulfah160922;
+
+/**
+ *
+ * @author ASUS
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World\n"+"MI 2C\n"+"Pemograman Berbasi Objek\n");
+        System.out.println("Nama\t : Ulfah Pratiwi\n"+"NoBp\t : 2101092022\n"+"Prodi\t : Manajemen Informatika\n"+"Juruusan : Teknologi Informasi\n"+"Alamat\t : Simpang Pasi");
+        System.out.println("");
+    }
+}
---
+++ src/Ulfah230922/Latihan13.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ulfah230922;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Latihan13 {
+    public static void main(String[] args){
+        int a = 1;
+        int b = 2;
+        int c = 1;
+        int d = 2;
+        int e = 1;
+        int f = 2;
+        int g = 1;
+        int h = 2;
+        int i = 1;
+        int r = 2;
+        int s = 1;
+        int t = 2;
+        int u = 1;
+        int v = 2;
+        int w = 1;
+        int x = 2;
+        int y = 1;
+        
+        
+      
+        
+        System.out.println("1. a/(b^(c^d))-e+f-(g*h)+i" );
+        System.out.println("2. (3*10)*(2/15)-2+(4^(2^2))" );
+        System.out.println("3. ((r^s)*t)/u)-v+(w^x)-y++" );
+    }
+}
