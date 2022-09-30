08bbdcdbe7b1f8233854ca6b1e834e0560febf25
UlfahPratiwi <ulfahpratiwi.bkt@gmail.com>
2022-09-30 11:52:44 +0700

    Create latihan4.java

---
+++ src/Ulfah300922/latihan4.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ulfah300922;
+
+/**
+ *
+ * @author ASUS
+ */
+import javax.swing.JOptionPane;
+
+public class latihan4 {
+   public static void main( String[] args ){ 
+        
+        int angka1 = 0;
+        int angka2 = 0;
+        int jumlah = 0;
+        
+        angka1 = Integer.parseInt(JOptionPane.showInputDialog("Masukkan angka 1")); 
+         
+        
+        angka2 =  Integer.parseInt (JOptionPane.showInputDialog("Masukkan angka 2")); 
+         
+        
+        jumlah = angka1 + angka2;
+          
+
+        JOptionPane.showMessageDialog(null, "jumlah = " + jumlah); 
+
+     }
+    
+}
