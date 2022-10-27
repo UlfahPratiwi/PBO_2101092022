70d3f371d2c8080a5659078228f006f7f4d1fac4
UlfahPratiwi <ulfahpratiwi.bkt@gmail.com>
2022-10-27 22:06:41 +0700

    Create Latihan2.java

---
+++ src/Ulfah211022/Latihan2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ulfah211022;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Latihan2 {
+     public static void main (String[] args){
+        
+        //do-while
+        
+         int r=0;
+        do{
+            System.out.println("Ulfah");
+            r++;
+        }while(true);
+    }
+}
