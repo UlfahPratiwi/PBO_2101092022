d499966f269fa527ec74b65e2b9fe670cbc9eae8
UlfahPratiwi <ulfahpratiwi.bkt@gmail.com>
2022-10-28 11:58:51 +0700

    Create Latihan2.java

---
+++ src/Ulfah281022/Latihan2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ulfah281022;
+
+/**
+ *
+ * @author ASUS
+ */
+
+public class Latihan2 {
+    public static void main(String[] args){
+        
+        //for loop - array
+        String days[] = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+        
+          for(int i=0; i<days.length; i++){
+            System.out.println(days[i]);
+        }
+          
+    }
+}
