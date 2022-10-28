b58bec04dc14cdff6792e056a14cb01836d61bc2
UlfahPratiwi <ulfahpratiwi.bkt@gmail.com>
2022-10-28 12:00:16 +0700

    Create Latihan7.java

---
+++ src/Ulfah281022/Latihan7.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ulfah281022;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Latihan7 {
+    public static void main(String[] args){
+        
+        //modul8
+        
+        double a = Double.parseDouble(args[2]);
+        double b = Double.parseDouble(args[3]);
+        
+        double sum,difference,product,quotient;
+        
+        sum = a + b;
+        difference = a - b;
+        product = a * b;
+        quotient = a / b ;
+        
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + difference);
+        System.out.println("Product = " + product);
+        System.out.println("Quotiennt = " + quotient);
+        
+    }
+}
