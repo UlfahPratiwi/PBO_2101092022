b8bb3dbe2d0101173adf52540045e908016fb9a0
Ulfah Pratiwi <114288917+UlfahPratiwi@users.noreply.github.com>
2022-12-02 21:37:51 +0700

    Add files via upload

--- src/Ulfah021222/BukuRecordExample.java
+++ src/Ulfah021222/BukuRecordExample.java
@@ -12,12 +12,33 @@ package Ulfah021222;
 public class BukuRecordExample {
      public static void main(String[] args){
         
+        // method accessor dan mutator
+        System.out.println("== Entry Buku Alamat ==" + "\n");
+        System.out.println("Accessor dan Mutator Method");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        BukuRecord buku1 = new BukuRecord();
+        
+        buku1.setName("Ulfah Pratiwi");
+        buku1.setAddress("Padang Panjang");
+        buku1.setNotelp("082384984501");
+        buku1.setEmail("ulfahpratiwi.bkt@gmail.com");
+    
+        System.out.println("Nama            : "+buku1.getName());
+        System.out.println("Alamat          : "+buku1.getAddress());
+        System.out.println("Nomor Telepon   : "+buku1.getNotelp());
+        System.out.println("Alamat E-Mail   : "+buku1.getEmail());
+        
+        System.out.println("====================================================");
+        
         // method constructor 
-        BukuRecord buku1 = new BukuRecord("Ulfah" , "Padang Panjang" , "082384984501" , "ulfahpratiwi.bkt@gmail.com" );
-        System.out.println("Nama                    :"+buku1.getName());
-        System.out.println("Alamat                  :"+buku1.getAddress());
-        System.out.println("No.Telpon               :"+buku1.getNotelp());
-        System.out.println("Alamat Email            :"+buku1.getEmail());
+        System.out.println("Constructor Method");
+         System.out.println("~~~~~~~~~~~~~~~~~~");
+        BukuRecord buku2 = new BukuRecord("Ulfah" , "Padang Panjang" , "082384984501" , "ulfahpratiwi.bkt@gmail.com" );
+        
+        System.out.println("Nama                    :"+buku2.getName());
+        System.out.println("Alamat                  :"+buku2.getAddress());
+        System.out.println("No.Telpon               :"+buku2.getNotelp());
+        System.out.println("Alamat Email            :"+buku2.getEmail());
      }
     
    
