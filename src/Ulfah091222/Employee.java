79cb9657c62a7781d30805d4673c7427f2c69b53
UlfahPratiwi <ulfahpratiwi.bkt@gmail.com>
2022-12-09 20:38:49 +0700

    Create Employee.java

---
+++ src/Ulfah091222/Employee.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ulfah091222;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Employee extends Person {
+    public String getName(){ 
+        System.out.println("Employee Name : " +name );
+        return name;
+    }
+}
