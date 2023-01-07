e3e68b20a1c702a3c1229b922ade93eb96dc620c
UlfahPratiwi <ulfahpratiwi.bkt@gmail.com>
2023-01-07 21:43:19 +0700

    Create AnggotaDao.java

---
+++ src/Ulfah/model/AnggotaDao.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ulfah.model;
+
+/**
+ *
+ * @author ASUS
+ */
+
+import java.util.List;
+
+public interface AnggotaDao {
+    Anggota save(Anggota anggota);
+    Anggota update(int index, Anggota anggota);
+    void delete(int index);
+    Anggota getAnggota(int index);
+    List<Anggota> getAllAnggota();
+}
